93a99a6f58c4e34f88df8cae4d38a7d5fde3eabc
Francesco Santini <francesco.santini@gmail.com>
2017-07-21 15:15:55 +0200

    Various improvements: *Making the transforms more general, acting on a number of shapes *Making the extension unit-agnostic *Fixing the width parameter which was constantly increased by sqrt(2) in case of no scaling

--- applytransform.py
+++ applytransform.py
@@ -12,6 +12,11 @@ import cubicsuperpath
 import math
 import simplestyle
 from simpletransform import composeTransform, fuseTransform, parseTransform, applyTransformToPath, applyTransformToPoint, formatTransform
+import re
+
+def ePrint(msg):
+    sys.stderr.write(str(msg) + "\n")
+    
 
 class ApplyTransform(inkex.Effect):
     def __init__(self):
@@ -39,6 +44,29 @@ class ApplyTransform(inkex.Effect):
 
         return node
 
+    # a dictionary of unit to user unit conversion factors - copied from inkex. We actually only need the unit names here
+    __uuconv = {'in': 96.0, 'pt': 1.33333333333, 'px': 1.0, 'mm': 3.77952755913, 'cm': 37.7952755913,
+                'm': 3779.52755913, 'km': 3779527.55913, 'pc': 16.0, 'yd': 3456.0, 'ft': 1152.0}
+
+    def getUnit(self, string):
+        unit = re.compile('(%s)$' % '|'.join(self.__uuconv.keys()))
+        u = unit.search(string)    
+        if u:
+            try:
+                return u.string[u.start():u.end()]
+            except KeyError:
+                pass
+        return self.getDocumentUnit()
+    
+    def getVal(self, string):
+        return self.unittouu(self.addDocumentUnit(string))
+    
+    def valWithUnit(self, val, unit):
+        if unit == self.getDocumentUnit():
+            return str(val)
+        else:
+            return str(self.uutounit(val, unit)) + unit
+
     def recursiveFuseTransform(self, node, transf=[[1.0, 0.0, 0.0], [0.0, 1.0, 0.0]]):
         transf = composeTransform(transf, parseTransform(node.get("transform", None)))
 
@@ -51,12 +79,14 @@ class ApplyTransform(inkex.Effect):
             update = False
 
             if 'stroke-width' in style:
-                stroke_width = self.unittouu(style.get('stroke-width').strip())
+                unit = self.getUnit(style.get('stroke-width').strip())
+                stroke_width = self.getVal(style.get('stroke-width').strip())
                 # pixelsnap ext assumes scaling is similar in x and y
                 # and uses the x scale...
                 # let's try to be a bit smarter
-                stroke_width *= math.sqrt(transf[0][0]**2 + transf[1][1]**2)
-                style['stroke-width'] = str(stroke_width)
+                stroke_width *= math.sqrt((transf[0][0]**2 + transf[1][1]**2)/2)
+
+                style['stroke-width'] = self.valWithUnit(stroke_width, unit)
                 update = True
 
             if update:
@@ -76,16 +106,114 @@ class ApplyTransform(inkex.Effect):
             points = points.strip().split(' ')
             for k,p in enumerate(points):
                 p = p.split(',')
-                p = [float(p[0]),float(p[1])]
+                unit = self.getUnit(p[0])
+                p = [self.getVal(p[0]),self.getVal(p[1])]
                 applyTransformToPoint(transf, p)
-                p = [str(p[0]),str(p[1])]
+                p = [self.valWithUnit(p[0],unit),self.valWithUnit(p[1],unit)]
                 p = ','.join(p)
                 points[k] = p
             points = ' '.join(points)
             node.set('points', points)
 
-        elif node.tag == inkex.addNS('rect', 'svg'):
-            node.set('transform', formatTransform(transf))
+        # if there is cx, there is also cy
+        if 'cx' in node.attrib:
+            cx = node.get('cx')
+            cy = node.get('cy')
+            unit = self.getUnit(cx)
+            pt = [self.getVal(cx), self.getVal(cy)]
+            applyTransformToPoint(transf, pt)
+            node.set('cx', self.valWithUnit(pt[0],unit))
+            node.set('cy', self.valWithUnit(pt[1],unit))
+            
+        if 'r' in node.attrib:
+            unit = self.getUnit(node.get('r'))
+            r = self.getVal(node.get('r'))
+            # this is a circle: is the scale uniform?
+            if transf[0][0] == transf[1][1]:
+                r *= abs(transf[0][0])
+                node.set('r', self.valWithUnit(r, unit))
+            else:
+                # transform is not uniform: go from circle to ellipse
+                # NOTE!!! Inkscape currently applies this particular transform as soon as we touch the object.
+                # therefore rx and ry are both assigned to r, otherwise the scaling is applied two times!
+                # this is kind of a bug of the implementation
+                rx = r #*abs(transf[0][0])
+                ry = r #*abs(transf[1][1])
+                node.set('rx', self.valWithUnit(rx, unit))
+                node.set('ry', self.valWithUnit(ry, unit))
+                del node.attrib['r']
+                node.tag = inkex.addNS('ellipse', 'svg')
+
+        if 'rx' in node.attrib:
+            unit = self.getUnit(node.get('rx'))
+            rx = self.getVal(node.get('rx'))*abs(transf[0][0])
+            ry = self.getVal(node.get('ry'))*abs(transf[1][1])
+            node.set('rx', self.valWithUnit(rx, unit))
+            node.set('ry', self.valWithUnit(ry, unit))
+            
+            
+        if 'x' in node.attrib: 
+            unit = self.getUnit(node.get('x'))
+            x = self.getVal(node.get('x'))*transf[0][0]
+            y = self.getVal(node.get('y'))*transf[1][1]
+            node.set('x', self.valWithUnit(x, unit))
+            node.set('y', self.valWithUnit(y, unit))
+            
+        if 'width' in node.attrib: 
+            unit = self.getUnit(node.get('width'))
+            w = self.getVal(node.get('width'))*transf[0][0]
+            h = self.getVal(node.get('height'))*transf[1][1]
+            if w < 0:
+                xUnit = self.getUnit(node.get('x'))
+                x = self.getVal(node.get('x'))
+                x += w;
+                w = -w;
+                node.set('x', self.valWithUnit(x, xUnit))
+            if h < 0:
+                yUnit = self.getUnit(node.get('y'))
+                y = self.getVal(node.get('y'))
+                y += h;
+                h = -h;
+                node.set('y', self.valWithUnit(y, yUnit))
+            node.set('width', self.valWithUnit(w, unit))
+            node.set('height', self.valWithUnit(h, unit))
+        
+
+
+        #elif node.tag == inkex.addNS('rect', 'svg'):
+            #node.set('transform', formatTransform(transf))
+            
+        #elif node.tag == inkex.addNS('circle', 'svg'):
+            #cx = node.get('cx')
+            #cy = node.get('cy')
+            #unit = self.getUnit(cx)
+            #pt = [self.getVal(cx), self.getVal(cy)]
+            #applyTransformToPoint(transf, pt)
+            #node.set('cx', self.valWithUnit(pt[0],unit))
+            #node.set('cy', self.valWithUnit(pt[1],unit))
+            #if 'r' in node.attrib:
+                #unit = self.getUnit(node.get('r'))
+                #r = self.getVal(node.get('r'))
+                ## this is a circle: is the scale uniform?
+                #if transf[0][0] == transf[1][1]:
+                    #r *= abs(transf[0][0])
+                    #node.set('r', self.valWithUnit(r, unit))
+                #else:
+                    ## transform is not uniform: go from circle to ellipse
+                    #rx = r*abs(transf[0][0])
+                    #ry = r*abs(transf[1][1])
+                    #del node.attrib['r']
+                    #node.set('rx', self.valWithUnit(rx, unit))
+                    #node.set('ry', self.valWithUnit(ry, unit))
+                    #node.tag = inkex.addNS('path', 'svg')
+            #else:
+                ## this is an ellipse
+                #unit = self.getUnit(node.get('rx'))
+                #rx = self.getVal(node.get('rx'))*abs(transf[0][0])
+                #ry = self.getVal(node.get('ry'))*abs(transf[1][1])
+                #node.set('rx', self.valWithUnit(rx, unit))
+                #node.set('ry', self.valWithUnit(ry, unit))
+            
 
         for child in node.getchildren():
             self.recursiveFuseTransform(child, transf)
